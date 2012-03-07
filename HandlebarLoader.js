17c4a878185e4804da8ad51066eace071501cd7e
Cyrille Bourgois <cyrille.bourgois@gmail.com>
2012-03-07 22:20:31 +0100

    Script creation

---
+++ HandlebarLoader.js
@@ -0,0 +1,111 @@
+//     HandlebarLoader.js 0.1
+
+//     (c) 2012 Cyrille Bourgois
+//     HandlebarLoader may be freely distributed under the MIT license.
+// 	   https://github.com/cbourgois/HandlebarLoader
+
+
+HandlebarLoader = function( options )
+{   
+	this._options = _.extend(
+		{
+			'baseUrl': 'tpl/',
+			'partialUrl': 'tpl/partials/',
+			'extension': 'html'
+		},
+		options
+	);
+	this._tpl = {};
+	this._templates = [];
+	this._partials = [];
+	this._callback = undefined;
+	
+};
+
+_.extend( HandlebarLoader.prototype, {
+	
+	loadTemplate : function ( index ) 
+	{
+        var name = this._templates[ index ];
+		var that = this;
+        $.ajax( {
+			url: that._options.baseUrl + name + '.' + that._options.extension, 
+			success: function (data) 
+			{ 
+                that._tpl[name] = function( params )
+				{          
+					var tpl = Handlebars.compile( data ); 
+					return tpl(params); 
+				};       
+            	index++;
+            	if ( index < that._templates.length )
+				{
+                	that.loadTemplate( index );
+            	} 
+				else if ( that._partials.length > 0 )
+				{ 
+					that.loadPartial( 0 );
+            	}
+				else if ( that._callback )
+				{   
+            		that._callback();    
+				}
+        	},
+			dataType: 'html' 
+		});
+    },
+
+	loadPartial: function ( index ) 
+	{
+		var name = this._partials[ index ];
+		var that = this;    
+		$.ajax( {
+			url: that._options.partialUrl + name + '.' + that._options.extension,  
+			success: function ( data ) 
+			{
+				Handlebars.registerPartial( name, data );
+            	index++;
+            	if ( index < that._partials.length )
+				{
+                	loadPartial(index);
+            	} 
+				else if ( that._callback )
+				{    
+            		that._callback();    
+				}
+            }, 
+			dataType: 'html' 
+		});
+	},
+
+	load: function ( nameTemplates, namePartials, callback ) {
+        
+		this._templates = nameTemplates;
+		this._partials = namePartials;
+		this._callback = callback;   
+		
+        if ( this._templates.length > 0 )
+		{
+        	this.loadTemplate( 0 );
+    	} 
+		else if ( this._partials.length > 0 )
+		{ 
+			this.loadPartial( 0 );
+    	}
+		else if ( this._callback )
+		{
+    		this._callback();    
+		}
+    },
+
+	getTemplate :function( name )
+	{
+		return this._tpl[ name ];
+	},
+	
+	getAllTemplates: function()
+	{
+		return this._tpl;
+	}
+    
+});
---
+++ README.md
@@ -0,0 +1,78 @@
+HandlebarLoader v0.1                
+=============
+
+This script help to load Handlebar templates and partials easily.
+
+How to use   
+-------
+
+### Initialisation ###
+
+    var loader = new HandlebarLoader();
+    loader.load( 
+    	[ 
+    		'template1', 
+    		'template2',
+    		'template3'
+    	], 
+    	[         
+    		'partial1'
+    	],  
+    	function()
+    	{   
+    		// templates are available...
+    	} 
+    );
+                    
+
+### To use a template ###
+            
+    loader.getTemplate( 'template1' )();
+
+### To use easily all templates ###
+
+    var templateContainer = loader.getAllTemplates();
+    templateContainer.template1();
+    templateContainer.template2( { foo: 'bar' } );
+       
+
+Options
+-------
+By default, the loader will load ``template1`` in ``tpl/template1.html`` and ``partial1`` in ``tpl/partials/partial1.html``.
+<table >
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Definition</th>
+      <th>Default value</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>baseUrl</td>
+      <td>Define templates folder</td>
+      <td><code>tpl/</code></td>
+    </tr>
+    <tr>
+      <td>partialUrl</td>
+      <td>Define partials folder</td>
+      <td><code>tpl/partials/</code></td>
+    </tr>
+    <tr>
+      <td>extension</td>
+      <td>Define template & partial extension</td>
+      <td><code>html</code></td>
+    </tr>
+  </tbody>
+</table>
+
+
+Define options at initialisation:
+
+    var loader = new HandlebarLoader( {
+        baseUrl: 'templates/',
+        partialUrl: 'partials/',
+        extension: 'hb'
+    } );
+
+Now, this loader will load ``template1`` in ``templates/template1.hb`` and ``partial1`` in ``partials/partial1.hb``.
